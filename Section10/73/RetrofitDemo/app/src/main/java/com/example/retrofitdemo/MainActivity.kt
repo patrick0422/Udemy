d23691624f0f9d8e0dff0a21326218c90bc3c3a7
patrick0422 <didxodnd0422@gmail.com>
2021-07-20 17:15:58 +0900

    20210720 Section 10 끝

--- Section10/73/RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ Section10/73/RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -13,45 +13,66 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var service: AlbumService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val service = RetrofitClient
+        service = RetrofitClient
             .getInstance()
             .create(AlbumService::class.java)
 
+//        getRequestWithQueryParameters()
+//        getRequestWithPathParameters()
+        uploadAlbum()
+    }
+    private fun getRequestWithQueryParameters() {
         val responseLiveData: LiveData<Response<Albums>> = liveData {
             val response = service.getSortedAlbums(3)
             emit(response)
         }
-
-        val pathResponse: LiveData<Response<AlbumsItem>> = liveData {
-            val response = service.getAlbum(3)
-            emit(response)
-        }
-        pathResponse.observe(this, Observer {
-            var title = it.body()?.title
-            Toast.makeText(applicationContext, title, Toast.LENGTH_SHORT).show()
-
-        })
-
-        responseLiveData.observe(this, Observer {
+        responseLiveData.observe(this, {
             val albumList = it.body()?.listIterator()
-
             if (albumList != null) {
                 while (albumList.hasNext()) {
                     val albumsItem = albumList.next()
                     val result =
                         " Album Title : ${albumsItem.title}\n" +
-                        " Album Id : ${albumsItem.id}\n" +
-                        " Album UserId : ${albumsItem.userId}\n\n"
+                                " Album Id : ${albumsItem.id}\n" +
+                                " Album UserId : ${albumsItem.userId}\n\n"
                     binding.tv1.append(result)
 
                     Log.i("MyTag", albumsItem.title)
                 }
             }
+        })
+    }
+    private fun getRequestWithPathParameters() {
+        val pathResponse: LiveData<Response<AlbumsItem>> = liveData {
+            val response = service.getAlbum(3)
+            emit(response)
+        }
+        pathResponse.observe(this, {
+            var title = it.body()?.title
+            Toast.makeText(applicationContext, title, Toast.LENGTH_SHORT).show()
+
+        })
+    }
+    private fun uploadAlbum() {
+        val album = AlbumsItem(101, "Hello!", 3)
+
+        val postResponse: LiveData<Response<AlbumsItem>> = liveData {
+            val response = service.uploadAlbum(album)
+            emit(response)
+        }
+        postResponse.observe(this, {
+            val receivedAlbumsItem = it.body()
+            val result =
+                " Album Title : ${receivedAlbumsItem?.title}\n" +
+                        " Album Id : ${receivedAlbumsItem?.id}\n" +
+                        " Album UserId : ${receivedAlbumsItem?.userId}\n\n"
+            binding.tv1.text = result
         })
     }
 }
--- Section10/73/RetrofitDemo/app/src/main/java/com/example/retrofitdemo/RetrofitClient.kt
+++ Section10/73/RetrofitDemo/app/src/main/java/com/example/retrofitdemo/RetrofitClient.kt
@@ -1,16 +1,30 @@
 package com.example.retrofitdemo
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RetrofitClient {
     companion object {
+        val interceptor= HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(25, TimeUnit.SECONDS)
+        }.build()
+
         val baseURL = "https://jsonplaceholder.typicode.com/"
 
         fun getInstance(): Retrofit {
             return Retrofit.Builder()
                 .baseUrl(baseURL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
                 .build()
         }
