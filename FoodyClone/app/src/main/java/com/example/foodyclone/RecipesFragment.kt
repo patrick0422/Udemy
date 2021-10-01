5dee4204eea5b400b2fee7ee3516ddee046f196a
양태웅 <didxodnd0422@gmail.com>
2021-10-01 16:26:39 +0900

    Sec 4: 14

---
+++ FoodyClone/app/src/main/java/com/example/foodyclone/RecipesFragment.kt
@@ -0,0 +1,24 @@
+package com.example.foodyclone
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.foodyclone.databinding.FragmentRecipesBinding
+
+class RecipesFragment : Fragment() {
+    private lateinit var binding: FragmentRecipesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recipes, container, false)
+
+        binding.recyclerview.showShimmer()
+
+        return binding.root
+    }
+}
