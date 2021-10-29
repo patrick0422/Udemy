81aeb4b3956be60b25c8624b627f7d467669bd75
양태웅 <didxodnd0422@gmail.com>
2021-10-29 15:39:32 +0900

    Foody 분리

--- FoodyClone/app/src/main/java/com/example/foodyclone/models/FoodRecipe.kt
+++
@@ -1,9 +0,0 @@
-package com.example.foodyclone.models
-
-
-import com.google.gson.annotations.SerializedName
-
-data class FoodRecipe(
-    @SerializedName("results")
-    val results: List<Result>
-)
